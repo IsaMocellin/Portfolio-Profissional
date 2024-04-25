48810c8e335494c786a2d0e9f49fe483431cea3c
isamocellin <isabelarequiaomocellin@hotmail.com>
2024-04-24 21:52:01 -0300

    Atualização da API

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,16 +1,42 @@
-async function fetchProfileData(){
+async function fetchProfileData() {
     const url = 'https://raw.githubusercontent.com/IsaMocellin/Portfolio-Profissional/main/data/profile.json';
     const fetching = await fetch(url)
     return await fetching.json()
 };
 
-function updateProfileInfo(profileData){
+function updateProfileInfo(profileData) {
     const photo = document.getElementById('profile.photo')
     photo.src = profileData.photo
     photo.alt = profileData.name
+    const name = document.getElementById('profile.name')
+    name.innerText = profileData.name
+    const job = document.getElementById('profile.job')
+    job.innerText = profileData.job
+    const location = document.getElementById('profile.location')
+    location.innerText = profileData.location
+    const phone = document.getElementById('profile.phone')
+    phone.innerText = profileData.phone
+    phone.href = `https://wa.me/${profileData.phone.replace(/\D/g, '')}`
+    const email = document.getElementById('profile.email')
+    email.innerText = profileData.email
+    email.href = `mailto${profileData.email}`
+}
+
+function updateSoftSkills(profileData){
+    const softSkills = document.getElementById('profile.skills.softsSkills')
+
+    softSkills.innerHTML = profileData.skills.softSkills.map(skill => `<li>${skill}</li>`).join('')
+}
+
+function updateHardSkills(profileData){
+    const hardSkills = document.getElementById('profile.skills.hardSkills')
+
+    hardSkills.innerHTML = profileData.skills.hardSkills.map(skill => `<li><img src="${skill.logo}" alt="${skill.name}" title="${skill.name}"></li>`).join('')
 }
 
 (async () => {
     const profileData = await fetchProfileData()
     updateProfileInfo(profileData)
+    updateSoftSkills(profileData)
+    updateHardSkills(profileData)
 })();
